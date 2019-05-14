44c1cc769e495a59460ff6f4a2acd2d91df81b26
Gonzalo2712 <47565549+Gonzalo2712@users.noreply.github.com>
2019-05-14 09:35:46 +0200

    Add files via upload

---
+++ cambiotema.php
@@ -0,0 +1,15 @@
+<?php # -*- coding: utf-8 -*-
+// Plugin Name: Cambio tema ULE-SIC-2019
+//Cambio prueba
+//add_filter( 'template_directory', 't5_switch_theme', 10, 3 );
+
+function switch_all_multisite_themes() {
+    foreach ( get_sites() as $site ) {
+        switch_to_blog( $site->blog_id );
+
+        switch_theme( 'ule-wp-2018' );
+
+        restore_current_blog();
+    }
+}
+switch_all_multisite_themes(); 
